0a8a9887d87cfb60efff31cb854cfe33c9cb74bc
ncc1701 <ncc170169@gmail.com>
2016-08-06 21:32:08 +0200

    Modified

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,7 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(publicado_fecha__lte=timezone.now()).order_by('publicado_fecha')
+	## posts = Post.objects.filter(publicado_fecha__lte=timezone.now()).order_by('publicado_fecha')
+	posts = Post.objects.all()
+
 	return render(request, 'blog/post_list.html', {'posts': posts})
